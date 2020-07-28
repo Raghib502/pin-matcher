0df90e41ca74301a840380e89f80daa4998e9e39
Raghib Ahsan <40718368+Raghib502@users.noreply.github.com>
2020-07-28 20:21:43 +0600

    done step 1

---
+++ script.js
@@ -0,0 +1,5 @@
+const errorSection = document.getElementById('error');
+const succeedSection = document.getElementById('succeed');
+
+errorSection.style.display ='none';
+succeedSection.style.display = 'none';
