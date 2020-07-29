c1a346cddb1d039ad041b9eb740db73562266e11
Raghib Ahsan <40718368+Raghib502@users.noreply.github.com>
2020-07-29 21:59:32 +0600

    part 2 done

--- script.js
+++ script.js
@@ -3,3 +3,11 @@ const succeedSection = document.getElementById('succeed');
 
 errorSection.style.display ='none';
 succeedSection.style.display = 'none';
+
+function randomNumber(min,max){
+    return Math.floor(Math.random()*(max-min+1)+min );
+}
+  
+function randomValue() {
+    document.getElementById('random-number').value = randomNumber(1000 , 9999);
+  }
